eccafecd68da7bc23b1f2bc1ebc09903dab491ec
Juan Pablo <juancho27392@hotmail.com>
2022-05-21 10:51:03 -0500

    Agregar archivos de proyecto.

---
+++ Semana7JT/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using Semana7JT.Views;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Semana7JT
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new NavigationPage (new Login());
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ Semana7JT/Database.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SQLite;
+
+namespace Semana7JT
+{
+    public interface Database
+    {
+        SQLiteAsyncConnection GetConnection();
+    }
+}
